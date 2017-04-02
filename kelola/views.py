7300cc3dca18544bd4e1d1f433d1022c3e1210c5
naufal <adaaja200@yahoo.com>
2017-04-02 12:20:13 +0700

    commit-2

--- kelola/views.py
+++ kelola/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
+#from .forms import PostForm
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'kelola/post_list.html', {'posts': posts})
+   # post = get_object_or_404(Post, pk = pk)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'kelola/post_detail.html', {'post': post})
+
+'''def post_new(request):
+    form = PostForm()
+    return render(request, 'kelola/post_edit.html', {'form': form})
 # Create your views here.
+'''

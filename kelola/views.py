1da7cc7327019cd201c19a1d0f1b9aa476f7efa8
naufal <adaaja200@yahoo.com>
2017-04-02 13:03:37 +0700

    commit-3

--- kelola/views.py
+++ kelola/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-#from .forms import PostForm
+from .forms import PostForm
 from .models import Post
 
 
@@ -16,8 +16,15 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'kelola/post_detail.html', {'post': post})
 
-'''def post_new(request):
-    form = PostForm()
-    return render(request, 'kelola/post_edit.html', {'form': form})
-# Create your views here.
-'''
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'kelola/post_edit.html', {'form': form})# Create your views here.

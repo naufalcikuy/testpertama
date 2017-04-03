89155bce726f57d228c0505f9e9e702c0c625a9e
naufal <adaaja200@yahoo.com>
2017-04-03 13:26:18 +0700

    commit-6

--- kelola/admin.py
+++ kelola/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post
+from .models import Knowledge
+from .models import BabyBio
 
 admin.site.register(Post)
+admin.site.register(Knowledge)
+admin.site.register(BabyBio)
 
 # Register your models here.
--- kelola/models.py
+++ kelola/models.py
@@ -17,5 +17,48 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Knowledge(models.Model):
+    category_choice = (
+        ('Health', 'health'),
+        ('Growing', 'growing'),
+        ('Development', 'development'),
+        ('Immunization', 'immunization'),
+    )
+
+    category = models.CharField(max_length=20, choices = category_choice)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class BabyBio (models.Model):
+    id_baby = models.AutoField(primary_key=True, unique=True)
+    baby_name = models.CharField(max_length=100)
+    date_birth = models.DateField(
+        blank=True, null=True
+    )
+    address = models.TextField()
+    mother_name = models.CharField(max_length=100)
+    father_name = models.CharField(max_length=100)
+    weight_birth = models.IntegerField()
+    height_birth = models.IntegerField()
+    headcircumference_birth = models.IntegerField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+
+
+    def __str__(self):
+        return self.baby_name
 
 # Create your models here.
